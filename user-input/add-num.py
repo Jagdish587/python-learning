0f5b77fbc00d2522429a31b2292770dfab38b087
Jagdish Tirumala <t.jag587@gmail.com>
2019-04-06 19:13:30 +0530

    created user-input

---
+++ user-input/add-num.py
@@ -0,0 +1,6 @@
+first_number = int ( input("Enter First No ") )
+second_number = int ( input("Enter Second No ") )
+
+print("first value = ", first_number, "second no = " , second_number)
+total = first_number + second_number
+print("total = ",total)
