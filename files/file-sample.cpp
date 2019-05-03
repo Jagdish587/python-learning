67250801d2e693fa2f9ad6004a8756092d035bb8
Jagdish Tirumala <t.jag587@gmail.com>
2019-05-03 22:54:33 +0530

    file sample example code

---
+++ files/file-sample.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+bool m_isRightTouch ;
+
+bool checksavedinstance()
+{
+		
+		ifstream fio("sample.txt", ios::app | ios::out | ios::in); 
+		int value ;
+		fio >> value ;
+		cout<<"from file = "<<value<<endl;	
+		
+		return value ;	
+		
+}
+
+void closepopup()
+{
+	ofstream outfile("sample.txt");
+	
+	outfile << m_isRightTouch ;
+	
+}
+
+int main()
+{
+		
+		if(!checksavedinstance()) {
+			cout<<"Left Steering Position Value \n ";
+			m_isRightTouch = 0;
+		}
+		else {
+			cout<<"Right Steering Position Value \n";
+			m_isRightTouch = 1;
+		}	
+		
+		m_isRightTouch = 1;
+					
+			
+		closepopup();
+		closepopup();
+		closepopup();
+				
+		return 0;
+
+}
