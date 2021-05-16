d436ae44d28b8cf80b62ece77a94223c1b2b8fc9
Jagdish587 <t.jag587@gmail.com>
2021-05-16 18:25:16 +0530

    python list example

---
+++ data-strcutures/list-example.py
@@ -0,0 +1,29 @@
+
+# create and print list 
+mylist = [1, "Jagdish", 2, "Sai Prasad", 2, 3, "Ajay", 3.14]
+print("value of list is ", mylist)
+
+#print len of list 
+print("length of list = ", len(mylist))
+
+#print by index
+print("value at pos 3 = {} {} " .format(mylist[3], mylist.index(3)))
+print("3 is present at index  = {} " .format(mylist.index(3)))
+
+#append a element in list 
+mylist.append(27)
+print(mylist)
+
+#Removes Sai Prasad elemnet from list
+mylist.remove("Sai Prasad")
+print(mylist)
+
+# pop's last element of list i.e 37 here 
+mylist.pop()
+print(mylist)
+
+#print elements ny for loop 
+for val in mylist:
+    print(val, end=" ")
+
+
