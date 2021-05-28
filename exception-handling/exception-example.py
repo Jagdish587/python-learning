a898f38d1bda7d565025e01def9dab83a384bb4d
Jagdish587 <t.jag587@gmail.com>
2021-05-28 22:57:15 +0530

    added some more example

--- exception-handling/exception-example.py
+++ exception-handling/exception-example.py
@@ -3,9 +3,14 @@ def mycalc():
 
     try:
         print("I am in try block")
-        #rm -rf raise NameError("Hi there , error occured")  # Raise Error
+        print("div value = ", k = 5//0)
+        raise NameError("Hi there , error occured")  # Raise Error
     except NameError as myerror:
         print("An exception occured ", myerror)
+    except ZeroDivisionError as zerror:
+        print("Zero dicision error occured ",zerror )
+    except:
+        print("some random exception ")
     else:
         print("entered in else cause")
     finally:
